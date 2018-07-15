ddc3f7d25e923c2a65bf41a0252e6087b898eb90
parkersalterego <parkersalterego92@gmail.com>
2018-07-14 18:26:07 -0700

    create and delete written and tested

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,7 @@
 module Api
     module V1
         class ArticlesController < ApplicationController
+
             def index
                 articles = Article.order('created_at DESC')
                 render json: {status: 'SUCCESS', message: 'Loaded articles', data: articles}, status: :ok
@@ -10,6 +11,27 @@ module Api
                 article = Article.find(params[:id])
                 render json: {status: 'SUCCESS', message: 'Loaded articles', data: article}, status: :ok
             end
+
+            def create
+                article = Article.new(article_params)
+
+                if article.save
+                    render json: {status: 'SUCCESS', message: 'Saved article', data: article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Article not saved', data: article.errors}, status: :unprocessable_entity 
+                end
+            end
+
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy()
+                render json: {status: 'SUCCESS', message: 'Article deleted',data: article }, status: :ok
+            end
+
+            private def article_params
+                params.permit(:title, :body)
+            end
+
         end
     end
 end
