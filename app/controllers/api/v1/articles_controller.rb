4a15452ce4809cf7c8365fdbdf55f4e820f17b33
parkersalterego <parkersalterego92@gmail.com>
2018-07-14 18:33:31 -0700

    full crud written and tested

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -28,6 +28,16 @@ module Api
                 render json: {status: 'SUCCESS', message: 'Article deleted',data: article }, status: :ok
             end
 
+            def update
+                article = Article.find(params[:id])
+
+                if article.update_attributes(article_params)
+                    render json: {status: 'SUCCESS', message: 'Updated article', data: article}, status: :ok
+                else
+                    render json: {status: 'ERROR', message: 'Article not updated', data: article.errors}, status: :unprocessable_entity 
+                end
+            end
+
             private def article_params
                 params.permit(:title, :body)
             end
